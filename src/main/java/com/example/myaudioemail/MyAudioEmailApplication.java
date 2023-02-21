6c0b202ab82c4e75fde3fc0d4e5bdf98e9f5f1a3
pedro <p.a.correia-de-sousa1@newcastle.ac.uk>
2023-02-21 02:20:04 +0000

    initial commit

---
+++ src/main/java/com/example/myaudioemail/MyAudioEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.myaudioemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAudioEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAudioEmailApplication.class, args);
+	}
+
+}
